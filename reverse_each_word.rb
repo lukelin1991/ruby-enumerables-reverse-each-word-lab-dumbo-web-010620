9c0fcf786c175f19d6beed52eddc0087508fe065
Luke Lin <lukelin1991@yahoo.com>
2019-12-10 18:51:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 def reverse_each_word(sentence)
   words_array = sentence.split(" ")
-  binding.pry
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
   new_array.join(" ")
